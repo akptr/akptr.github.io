c58956109ff9f2b2f7cd7ea8cae1f6f11b4a7638
akptr <akbarnoer7th@outlook.de>
2018-11-23 03:44:38 +0700

    second changed

--- profile/js/main.js
+++ profile/js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Akbar Putra.", "first man of four.", "Sagitarrius.", "part of history AG."],
       typeSpeed: 100,
       loop: true,
     });
